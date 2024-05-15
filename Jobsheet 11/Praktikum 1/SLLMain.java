3020ddbc7c0dd20b463cf6531d384a9b93c301c4
Satrio Dian Nugroho <143977344+SATRIODIANNUGROHO@users.noreply.github.com>
2024-05-15 13:19:44 +0700

    Praktikum 1

---
+++ Jobsheet 11/Praktikum 1/SLLMain.java
@@ -0,0 +1,15 @@
+package com.jobsheet11.praktikum01;
+public class SLLMain {
+    public static void main(String[] args) {
+        LinkedList myLinkedList = new LinkedList();
+        myLinkedList.print();
+        myLinkedList.addFirst(800);
+        myLinkedList.print();
+        myLinkedList.addFirst(700);
+        myLinkedList.print();
+        myLinkedList.addLast(500);
+        myLinkedList.print();
+        myLinkedList.insertAfter(700, 300);
+        myLinkedList.print();
+    }
+}
